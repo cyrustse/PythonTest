3563cf64db48f26ddd494e573ed33e806a2d7dd7
cyrustse <tsechunhong@gmail.com>
2018-02-02 16:54:17 +0800

    Update AnalyseJobs.py

--- AnalyseJobs.py
+++ AnalyseJobs.py
@@ -1,12 +1,12 @@
 import pandas as pd;
 
-df = pd.read_csv('2018-02-01.csv');
+df = pd.read_csv('2018-02-02.csv');
 df_com = pd.read_csv('Company.csv');
 # print(df.head(3));
 
 def cleanAgentCompany(df):
     _df = df;
-    _removeComp = "Consult|recruit|Person|Search|Conn|Solution|Online|db |Tech|Info|Inno|Talent|Agent|Excel|Career|McKinley|Partner|Robert Walters|George Samuel|Wellesley Associates|HKT|Asian TAT Limited|Statsmaster";
+    _removeComp = "Consult|recruit|Person|Search|Conn|Solution|Online|db |Tech|Info|Inno|Talent|Agent|Excel|Career|McKinley|Partner|Robert Walters|George Samuel|Wellesley Associates|HKT|Asian TAT Limited|Statsmaster|Software|Lazybug|eRun|Exactal Limited";
     _df.loc[(_df['Company'].str.contains(_removeComp, case=False)) & ~(_df['Type'] == 'Agent') & ~(_df['Type'] == 'Company'), 'Type'] = 'Agent';
     return  _df;
 
@@ -15,14 +15,14 @@ def cleanAgentCompany(df):
 
 
 def main():
-    print('Hello World!');
+    _SearchName ='watson';
     df_v2 = pd.merge(df, cleanAgentCompany(df_com), on='Company', how='left');
     # print(df_v2);
     # df_v2.to_csv('2018-02-01_v2.csv', index=False)
     # print(df_v2.head(3));
     # print(df_v2.dtypes);
-    print(df_v2[~(df_v2['Type'] == 'Agent')].head(5));
-
+    # print(df_v2[~(df_v2['Type'] == 'Agent')].head(5));
+    print(df_v2[~(df_v2['Type'] == 'Agent') & (df_v2['Company'].str.contains(_SearchName, case=False))].head(5));
 
 if __name__ == "__main__":
     main();
